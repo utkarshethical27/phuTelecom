7f7daaaf16d2de5fb0a7344ec984a45abc2575fd
utkarshethical27 <136327443+utkarshethical27@users.noreply.github.com>
2023-09-07 20:31:24 +0530

    Update uploadFile.js

--- src/middlewares/uploadFile.js
+++ src/middlewares/uploadFile.js
@@ -1,6 +1,7 @@
 const fileUpload = require('express-fileupload')
 const path = require('path')
 const Client = require('ftp')
+const fs = require('fs')
 
 const upload = async (req,res,next)=>{
     try{
@@ -8,21 +9,24 @@ const upload = async (req,res,next)=>{
         req.files.file.mv('./storage/'+name,(err)=>{
             if(err) throw err
         })
-        const server = new Client()
-        server.on('ready',async ()=>{
-            const file = path.join(__dirname,'../../storage/'+name)
-            await server.put(file,'./Storage/',(err)=>{
-                if(err) throw err
-                res.render('upload',{
-                    message: 'File uploaded successfully'
+        fs.readFile(name,(err, data)=>{
+            if(err) console.log(err)
+            const server = new Client()
+            server.on('ready',async ()=>{
+                const file = path.join(__dirname,'../../storage/'+name)
+                await server.put(data,'./Storage/',(err)=>{
+                   if(err) console.log(err)
+                    res.render('upload',{
+                        message: 'File uploaded successfully'
+                    })
                 })
             })
-        })
-        server.connect({
-            host: 'ftpupload.net',
-            user: 'if0_34979074',
-            password: 'mqwiH8x16sv'
-        })
+            server.connect({
+                host: 'ftpupload.net',
+                user: 'if0_34979074',
+                password: 'mqwiH8x16sv'
+            })
+        }
     }catch(e){
         res.render('upload',{
             message: 'File upload failed'
