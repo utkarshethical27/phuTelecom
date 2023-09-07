06f9e77f8a59468b577df6c0a0a073c675c8ecb0
utkarshethical27 <136327443+utkarshethical27@users.noreply.github.com>
2023-09-07 11:33:28 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -25,7 +25,7 @@ app.use(express.urlencoded({extended:true}))
 app.use('/phus', phusRoutes)*/
 app.get('',(req,res)=>{
     res.render('down')
-}
+})
 require('dotenv').config()
 app.use(fileUpload())
 
