4e6786637bb822179916ef07e68cf91ac626c1a5
utkarshethical27 <136327443+utkarshethical27@users.noreply.github.com>
2023-09-07 20:42:27 +0530

    Update uploadFile.js

--- src/middlewares/uploadFile.js
+++ src/middlewares/uploadFile.js
@@ -9,11 +9,11 @@ const upload = async (req,res,next)=>{
         req.files.file.mv('./storage/'+name,(err)=>{
             if(err) throw err
         })
-        fs.readFile(name,(err, data)=>{
+        const file = path.join(__dirname,'../../storage/'+name)
+        fs.readFile(file,(err, data)=>{
             if(err) console.log(err)
             const server = new Client()
             server.on('ready',async ()=>{
-                const file = path.join(__dirname,'../../storage/'+name)
                 await server.put(data,'./Storage/',(err)=>{
                    if(err) console.log(err)
                     res.render('upload',{
