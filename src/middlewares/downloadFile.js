f9c9a8bce0ebfb9ed195dd03f0914c064223e6ba
utkarshethical27 <136327443+utkarshethical27@users.noreply.github.com>
2023-09-08 18:32:13 +0530

    Create downloadFile.js

---
+++ src/middlewares/downloadFile.js
@@ -0,0 +1,15 @@
+const ftp = require('basic-ftp')
+
+const download = (req,res)=>{
+    const client = new ftp.Client()
+    client.ftp.verbose = true
+    try {
+        await client.access({
+            host: "ftpupload.net",
+            user: "if0_34989307",
+            password: "BAW94rV25CA"
+        })
+      await client.downloadTo(
+}
+
+module.exports = download
