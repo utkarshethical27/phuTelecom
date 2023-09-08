95fedc6a9462f9e2f53ef164f21b8c3948b8eefc
utkarshethical27 <136327443+utkarshethical27@users.noreply.github.com>
2023-09-08 15:06:57 +0530

    Update uploadFile.js

--- src/middlewares/uploadFile.js
+++ src/middlewares/uploadFile.js
@@ -1,6 +1,6 @@
 const fileUpload = require('express-fileupload')
 const path = require('path')
-const Client = require('ftp')
+const ftp = require('basic-ftp')
 const fs = require('fs')
 
 const upload = async (req,res,next)=>{
@@ -10,22 +10,20 @@ const upload = async (req,res,next)=>{
             console.log('File moved')
         })
         const file = path.join(__dirname,'../../storage/'+name)
-        const server = new Client()
-        server.on('ready',async ()=>{
-            await server.put(file,'/storage/',(err)=>{
-                if(err) res.send(err)
-                else{
-                    res.render('upload',{
-                        message: 'File uploaded successfully'
-                    })
-                }
+        const client = new ftp.Client()
+        client.ftp.verbose = true
+        try {
+            await client.access({
+                host: "ftpupload.net",
+                user: "if0_34989307",
+                password: "BAW94rV25CA",
+                secure: true
             })
-        })
-        server.connect({
-            host: 'ftpupload.net',
-            user: 'if0_34989307',
-            password: 'BAW94rV25CA'
-        })
+        await client.uploadFrom(file,name)
+    }
+    catch(err) {
+        console.log(err)
+    }
     }catch(e){
         res.render('upload',{
             message: 'File upload failed'
