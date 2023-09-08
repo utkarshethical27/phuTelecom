3e80bc8c660d60e2526301038ed08b70a0544678
utkarshethical27 <136327443+utkarshethical27@users.noreply.github.com>
2023-09-08 14:40:37 +0530

    Update uploadFile.js

--- src/middlewares/uploadFile.js
+++ src/middlewares/uploadFile.js
@@ -7,19 +7,19 @@ const upload = async (req,res,next)=>{
     try{
         const name = req.body.name
         await req.files.file.mv('./storage/'+name,()=>{
+            console.log('File moved')
         })
         const file = path.join(__dirname,'../../storage/'+name)
         const server = new Client()
         server.on('ready',async ()=>{
             await server.put(file,'htdocs/',(err)=>{
-            if(err) res.send(err)
-            else{
-            res.render('upload',{
-             message: 'File uploaded successfully'
-                 })
-            }
+                if(err) res.send(err)
+                else{
+                    res.render('upload',{
+                        message: 'File uploaded successfully'
+                    })
+                }
             })
-            
         })
         server.connect({
             host: 'ftpupload.net',
