1ced8e4df5c80b5ec8f191daa4fd60eff8a52d86
utkarshethical27 <136327443+utkarshethical27@users.noreply.github.com>
2023-09-11 11:56:34 +0530

    Update readDir.js

--- src/middlewares/readDir.js
+++ src/middlewares/readDir.js
@@ -11,18 +11,18 @@ const read = async (req,res,next)=>{
             user: "if0_34989307",
             password: "BAW94rV25CA"
         })
-    const dir = await client.list('')
-    res.send(dir)
-    /*if(err) res.send(err)
-    let files = []
-    dir.splice(dir.indexOf('chatHistory.txt'),1)
-    dir.forEach((e)=>{
-        files.push(e.name)
-    })
-    res.render('download',{
-        files: files
-    })*/
-  }
+        const dir = await client.list('')
+        res.send(dir)
+        /*if(err) res.send(err)
+        let files = []
+        dir.splice(dir.indexOf('chatHistory.txt'),1)
+        dir.forEach((e)=>{
+            files.push(e.name)
+        })
+        res.render('download',{
+            files: files
+        })*/
+    }
   catch(e){
       res.send(e)
   }
