a4331ac6a41d572bb181862d492147c76795338b
utkarshethical27 <136327443+utkarshethical27@users.noreply.github.com>
2023-09-11 11:53:28 +0530

    Update readDir.js

--- src/middlewares/readDir.js
+++ src/middlewares/readDir.js
@@ -1,27 +1,31 @@
 const fs = require('fs')
 const path = require('path')
-const Client = require('ftp')
+const ftp = require('basic-ftp')
 
 const read = async (req,res,next)=>{
-    const server = new Client()
-    server.on('ready',async ()=>{
-        server.list('',(err,dir)=>{
-            if(err) res.send(err)
-            let files = []
-            dir.splice(dir.indexOf('chatHistory.txt'),1)
-            dir.forEach((e)=>{
-                files.push(e.name)
-            })
-            res.render('download',{
-                files: files
-            })
+    const client = new ftp.Client()
+    client.ftp.verbose = true
+    try {
+        await client.access({
+            host: "ftpupload.net",
+            user: "if0_34989307",
+            password: "BAW94rV25CA"
+        })
+    await client.list('',(err,dir)=>{
+        if(err) res.send(err)
+        let files = []
+        dir.splice(dir.indexOf('chatHistory.txt'),1)
+        dir.forEach((e)=>{
+            files.push(e.name)
+        })
+        res.render('download',{
+            files: files
         })
     })
-    server.connect({
-        host: 'ftpupload.net',
-        user: 'if0_34989307',
-        password: 'BAW94rV25CA'
-    })
+  }
+  catch(e){
+      res.send(e)
+  }
 }
 
 module.exports = read
